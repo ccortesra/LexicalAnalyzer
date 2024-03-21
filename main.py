fd93058c650dc9c5434b09bbdd26f9dd5c433685
Carlos Cortes <ccortesra@unal.edu.co>
2024-03-21 18:29:45 -0500

    Solving \n issue

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import re
 
-def read_text_from_file(filename):
-  with open(filename, 'r') as file:
-      text = file.read()
-  return text
-
-def write_text_to_file(text, filename):
-  with open(filename, 'w') as file:
-      file.write(text)
-
-
 class LexicalAnalyzer:
   def __init__(self,text_lines):
     self.text_lines = text_lines
-    self.reserved_words = ['escribir','caso','verdadero',   'falso','mientras','rango','sino','si','hasta','repetir', 'cierto', 'falso', 'imprimir', 'leer', 'retornar', 'funcion', 'en', 'para','fin']
+    self.reserved_words = ['fun', 'escribir', 'poner','caso','verdadero', 'desde', 'falso','mientras','rango','sino','si','hasta','repetir', 'cierto', 'falso', 'imprimir', 'leer', 'retornar', 'funcion', 'en', 'para','fin', 'retorno']
     self.operators = [
     ("tkn_and", "&&"),
     ("tkn_or", "\\|\\|"),
-    ("tkn_concat", "\\.."),
+    ("tkn_concat", "\\.\\."),
     ("tkn_period", "\\."),
     ("tkn_comma", ","),
     ("tkn_semicolon", ";"),
@@ -51,7 +41,7 @@ class LexicalAnalyzer:
     ("tkn_assign", "="),
     ("tkn_not", "!")
 ]
-    self.real_exp = r'[0-9]*.[0-9]*'
+    self.num_exp = r'\d+(\.\d+)?'
     self.variable_exp = r'[a-zA-Z_][0-9a-zA-Z_]*'
 # ------------------------------------------------------------------------------------------
   def find_reserved_words(self, line, i, j):
@@ -65,21 +55,29 @@ class LexicalAnalyzer:
             # Verificar si el inicio coincide con el índice actual 'j'
             if inicio == j:
                 texto_coincidente = coincidencia.group()  # Obtener el texto que coincide
-                print(f'<{texto_coincidente},{i+1},{j+inicio+1}>')
-                j += coincidencia.end() - coincidencia.start()
+                print(f'<{texto_coincidente},{i+1},{j+1}>')
+                j += (coincidencia.end()-inicio)
                 return j
     return j
+  
+  def find_numbers(self,line,i,j):
+    coincidencia = re.match(self.num_exp, line[j:])
+    if coincidencia:
+      inicio = coincidencia.start()  # Obtener la posición de inicio de la coincidencia
+      texto_coincidente = coincidencia.group()  # Obtener el texto que coincide
+      print(f'<tkn_real,{texto_coincidente},{i+1},{j+inicio+1}>')
+      j += coincidencia.end() 
+      return j
+    return j
 
-      
   def find_operators(self,line,i,j):
     for op, exp in self.operators:
       coincidencia = re.match(exp, line[j:])
       if coincidencia:
         inicio = coincidencia.start()  # Obtener la posición de inicio de la coincidencia
-        texto_coincidente = coincidencia.group()  # Obtener el texto que coincide
-        print(f'<{op},{texto_coincidente},{i+1},{j+inicio+1}>')
+        print(f'<{op},{i+1},{j+inicio+1}>')
         j += coincidencia.end()
-        return j
+        return j 
     return j
 
   def find_identifiers(self,line,i,j):
@@ -91,57 +89,90 @@ class LexicalAnalyzer:
       j += coincidencia.end()
       return j
     return j
+  
+  def find_tkn_string(self,line,i,j):
+    if line[j-1] == '"':
+      coincidencia = re.match(r'.*?(?=")', line[j:])
+    else:
+      coincidencia = re.match(r".*?(?=')", line[j:])
+    if coincidencia:
+      inicio = coincidencia.start()  # Obtener la posición de inicio de la coincidencia
+      texto_coincidente = coincidencia.group()  # Obtener el texto que coincide
+      print(f'<tkn_str,{texto_coincidente},{i+1},{j+inicio}>')
+      j += coincidencia.end()
+      return j+1
+    else:
+      return j-1
 
+  
+  # ----------------------------------------------------------------
+  # FUNCIÓN PRINCIPAL
+  # ----------------------------------------------------------------
   def find_tokens(self):
+    multi_comment = False
     for i in range(len(self.text_lines)):
       line = self.text_lines[i]
-      if line == '':
+      if line.strip() == '':
         continue
-      if line.startswith('//'):  # Ignorar comentarios
-        continue
-      if line.startswith('#'):  # Ignorar comentarios
-        continue
-      
-      
+      # if line.startswith('//'):  # Ignorar comentarios
+      #   continue
+      # if line.startswith('#'):  # Ignorar comentarios
+      #   continue
+
+
 
       j = 0
-    
+
       while j < len(line):
         # Palabras reservadas
         try:
-          if line[j] == ' ':
+          if line[j:].startswith('*/'):  # Ignorar comentarios
+            multi_comment = False
+            j+=2  
+            continue
+        
+          elif line[j:].startswith('/*') or multi_comment:  # Ignorar comentarios
+            multi_comment = True
+            j+=1 # Aqui vale la pena seguir mirando la misma linea?
+            continue
+
+          
+
+          elif line[j:].startswith('//'):  # Ignorar comentarios
+            break
+          elif line[j:].startswith('#'):  # Ignorar comentarios
+            break
+
+          elif line[j] == ' ':
             j+= 1
             continue
-          j = self.find_reserved_words(line,i,j)
 
-          # Números
-          if re.match('\d', line[j]):
-            coincidencia = re.match(self.real_exp, line[j:])
-            inicio = coincidencia.start()  # Obtener la posición de inicio de la coincidencia
-            texto_coincidente = coincidencia.group()  # Obtener el texto que coincide
-            print(f'<tkn_real,{texto_coincidente},{i+1},{j+inicio+1}>')
-            j += coincidencia.end() - 1
+          elif line[j] == '"' or line[j] == "'":
+            starting_j = j
+            j = self.find_tkn_string(line,i,j+1)
+            if j == starting_j:
+              print(f'>>> Error lexico (linea: {i+1}, posicion: {j+1})')
+              return
 
-          # Operadores
-          j = self.find_operators(line,i,j)
-
-          j = self.find_identifiers(line,i,j)
-
+          else:
+            starting_j = j
+            j = self.find_reserved_words(line,i,j)
+            j = self.find_numbers(line,i,j)
+            j = self.find_operators(line,i,j)
+            j = self.find_identifiers(line,i,j)
+            if starting_j == j:
+              print(f'>>> Error lexico (linea: {i+1}, posicion: {j+1})')
+              return
 
         except:
           continue
 
+
 if __name__ == '__main__':
-  #input_text = open('./case 1.txt','r')
-#   input_line = """hola()                              #
-# escribir(nombreFuncion(4.56,53.98))"""
-  input_line = """// Este caso evalua palabras reservadas básicas
-
-escribir verdadero
-
-cierto falso mientras rango
-
-sino si hasta repetir caso"""
+  input_line = """para i en rango(10)
+poner(i .. '\n')
+fin
+"""
   #input_line = input()
   input_line = input_line.split('\n')
   lexical_analyzer = LexicalAnalyzer(input_line)
